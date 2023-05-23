5f13219d1bf23ed70560ce157d442685a272f0d3
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-05-23 16:01:40 -0700

    simple shell

---
+++ path.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * path_finder - this function finds the path and return it
+ * @tokens: the command.
+ * @s: the shell name.
+ * @count:the number of command
+ * Return: NULL if path is not found or if an error occured,
+ * or return THE path with the command.
+ */
+
+char *path_finder(char *tokens, char *s, int count)
+{
+	char *dir, *full_path, *p, *sl, *command = _strdup(tokens);
+	char *path = _getenv("PATH"), *command_path = NULL;
+
+	if (path == NULL || _strcmp(path, "") == 0)
+	{
+		if (access(command, X_OK | F_OK) == 0)
+		{
+			return (command);
+		}
+		else
+		{
+			_error(command, s, count);
+			exit(127);
+		}
+	}
+	while (*path)
+	{
+		dir = malloc(MAX_TOKEN_LENGTH);
+		p = _strchr(path, ':');
+		if (p == NULL)
+		{
+			_strcpy(dir, path);
+			path += _strlen(path);
+		}
+		else
+		{
+			_strncpy(dir, path, p - path);
+			dir[p - path] = '\0';
+			path = p + 1;
+		}
+		sl = _strchr(command, '/');
+		if (sl == NULL)
+		{
+			full_path = malloc(MAX_TOKEN_LENGTH);
+			_sprintf(full_path, "%s/%s", dir, command);
+		}
+		else
+			full_path = _strdup(command);
+		if (access(full_path, X_OK | F_OK) == 0)
+		{
+			command_path = _strdup(full_path);
+			free(dir), free(full_path);
+			break;
+		}
+		free(dir), free(full_path);
+	}
+	if (command_path == NULL)
+	{
+		_error(command, s, count), free(command);
+		exit_status = 127;
+		return (NULL);
+	}
+	free(command);
+	return (command_path);
+}
---
+++ strfun.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @str1: the first string
+ * @str2: the seocnd string
+ * Return: 0 if both string are same, otherwise the diff btw str1 and str2
+ */
+
+int _strcmp(char *str1, char *str2)
+{
+	int idx = 0;
+
+	while (str1[idx] != '\0' && str2[idx] != '\0' && str1[idx] == str2[idx])
+		idx++;
+
+	return (str1[idx] - str2[idx]);
+}
+
+/**
+ * _strcpy - copy a string into another string
+ * @dest: the location
+ * @src: the string to copy
+ * Return: a pointer to the copied string.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int idx = 0;
+
+	while (src[idx] != '\0')
+	{
+		dest[idx] = src[idx];
+		idx++;
+	}
+	dest[idx] = '\0';
+	return (dest);
+}
+
+/**
+ * _strncpy - copy's n number of character from one string to another
+ * @dest: the string we copy to
+ * @src: the string to come from
+ * @num: the number of string to copy
+ * Return: a pointer to copied string
+ */
+
+char *_strncpy(char *dest, const char *src, size_t num)
+{
+	size_t idx;
+
+	for (idx = 0; idx < num && src[idx] != '\0'; idx++)
+	{
+		dest[idx] = src[idx];
+	}
+	for (; idx < num; idx++)
+	{
+		dest[idx] = '\0';
+	}
+	return (dest);
+}
+
+/**
+ * _strdup - duplicates a string
+ * @src: the string to duplicate
+ * Return: a pointer to the copied string
+ */
+
+char *_strdup(char *src)
+{
+	size_t idx, len = _strlen(src);
+	char *dest = malloc(len + 1);
+
+	if (dest == NULL)
+		return (NULL);
+	for (idx = 0; idx <= len; idx++)
+		dest[idx] = src[idx];
+	return (dest);
+}
+
+/**
+ * _strlen - computes the lenght of a string
+ * @str: the string
+ * Return: the lenght of a string
+ */
+
+int _strlen(char *str)
+{
+	size_t count = 0;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+	while (str[count] != 0)
+		count++;
+	return (count);
+}
